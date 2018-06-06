041fa1c94474646e1545873d5a949839aadbcc51
robsonsjre <robsonsjre@gmail.com>
2018-06-06 02:11:04 -0300

    added jsx file

---
+++ src/jsx.js
@@ -0,0 +1,10 @@
+function SimplePage() {
+  const name = 'Rob'
+  return (
+    <div className="Main">
+      <p> This is a P</p>
+      <button> Sou um botão</button>
+      <p> o nome da variável é {name}</p>
+    </div>
+  )
+}
